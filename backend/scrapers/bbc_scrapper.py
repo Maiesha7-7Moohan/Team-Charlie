c3273fbd5dc9596a566c9935ca5141d19207505b
khanyagcilitshane <khanyagcilitshane@gmail.com>
2026-07-23 11:33:08 +0200

    Added BBC raw scraped datastorage

--- backend/scrapers/bbc_scrapper.py
+++ backend/scrapers/bbc_scrapper.py
@@ -1,26 +1,55 @@
 from bs4 import BeautifulSoup
 import requests
+import json
 
 url = "https://feeds.bbci.co.uk/news/rss.xml"
 
 response = requests.get(url)
 
-# Check if the request was successful
 if response.status_code == 200:
-    soup = BeautifulSoup(response.content, "xml")
+
+    soup = BeautifulSoup(response.content,"xml")
 
     items = soup.find_all("item")
 
-    for item in items:
-        title = item.title.text
-        description = item.description.text
-        link = item.link.text
-        pub_date = item.pubDate.text
+    articles = []
 
-        print(f"Title: {title}")
-        print(f"Description: {description}")
-        print(f"Link: {link}")
-        print(f"Published: {pub_date}")
-        print("-" * 50)
+    for item in items:
+        article ={
+            "title": item.title.text,
+            "description": item.description.text,
+            "link": item.link.text,
+            "published": item.pubDate.text
+        }
+
+        articles.append(article)
+
+    with open(
+        "data/raw/bbc_raw.json",
+        "w",
+        encoding="utf-8"
+    ) as file:
+
+        json.dump(
+            articles,
+            file,
+            indent=4,
+            ensure_ascii=False
+        )
+
+    print(
+        f"Successfully saved {len(articles)} articles"
+        "as raw JSON."
+    )
+
+
 else:
-    print(f"Failed to fetch RSS feed. Status code: {response.status_code}")
+
+    print(
+        f"Failed to fecth RSS feed. "
+        f"Status code: {response.status_code}"
+    )
+
+        
+
+
